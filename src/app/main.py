4b2fff84fc0cab68adfe96e46c164b017bb0dbb0
tsuna244 <131971495+tsuna244@users.noreply.github.com>
2025-03-30 00:02:37 +0100

    merge initial setup branch into develop. got accidentally merged into main
    * initial commit with requirements.txt
    
    * changed main.py to be basic fastapi template
    
    * changed folder references inside of main and finished dockerfile
    
    * added pokeapi example with pokebase
    
    * created docker image building pipeline as example
    
    * added /item in fastapi application to test ci cd pipeline
    
    * changed ci_cd.yml path to .github/workflows
    
    * fixed cicd.yml bei reformatting it
    
    * changed cicd so it only builds on main and develop
    
    * created pull_request ci cd attempt
    
    * added basic test.py for fastapi
    
    * changed test.py path inside pull_request.yml
    
    * added style sheet, base and navbar templates example. Added pokeapi to main without example
    
    * added pokeapi to navbar.html, added/updated test.py, fixed issue in template navbar regarding pokemon icon

---
+++ src/app/main.py
@@ -0,0 +1,29 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from pydantic import BaseModel
+
+from pokeapi import *
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/")
+async def read_root(request: Request):
+    return {"Test": "Test"}
+
+@app.get("/pokesprite")
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        name="base.html",
+        request=request, 
+        context={"brand": get_pokesprite_url_by_id(444)}
+    )
