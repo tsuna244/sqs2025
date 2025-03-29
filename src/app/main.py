8f73dc9625d4ab09038a869966b095b953d7ad75
tsuna244 <gulcinoemre@gmail.com>
2025-03-29 13:06:22 +0100

    changed main.py to be basic fastapi template

--- src/app/main.py
+++ src/app/main.py
@@ -16,16 +16,6 @@ templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def read_root(request: Request):
-    return templates.TemplateResponse(
-        name="base.html",
-        request=request, 
-        context={"brand": get_pokesprite_url_by_id(222)}
-    )
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse(
-        request=request, name="head.html", context={"id": id, "brand": get_pokesprite_url_by_id(222)}
-    )
+    return {"Test": "Test"}
