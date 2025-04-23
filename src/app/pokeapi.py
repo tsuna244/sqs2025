5d5ac8387cc8a490f4af12b1056c773b96498eff
tsuna244 <131971495+tsuna244@users.noreply.github.com>
2025-04-23 10:35:34 +0200

    setup arc42 with readthedocs. started changes for pokeapi
    * removed exampletext from template
    
    * added architecture.rst and changed pokeapi to inlcude a pokemon obj
    
    * changed pokeapi caching to use the cache function from pokebase

--- src/app/pokeapi.py
+++ src/app/pokeapi.py
@@ -9,8 +9,33 @@ SPRITE_POKEMON_CACHE = "sprite/pokemon/"
 
 pb.cache.set_cache(CACHE_DIR)
 
-def get_pokesprite_url_by_id(id: int):
-    file_name = SPRITE_POKEMON_CACHE + str(id) + ".png"
-    if not os.path.exists(CACHE_DIR + file_name):
+class PokemonObj():
+    poke_id: int
+    name: str
+    gen: str
+
+    sprite_path: str
+
+def get_generation():
+    print(pb.APIResource("generation", "generation-i"))
+
+def read_dat_file():
+    print(pb.cache.load("generation", 7))
+
+def get_sprite_by_id(id: int):
+    #pb.SpriteResource('pokemon', 69)
+    print()
+
+def get_pokesprite_url_by_id(id: int, depth: int):
+    if depth > 1:
+        raise Exception("Could not load Pokemon sprite")
+    try:
+        return pb.cache.load_sprite("pokemon", id)["path"].split("static")[1].replace("\\", "/")
+    except FileNotFoundError as e:
         pb.SpriteResource('pokemon', id)
-    return ".cache/" + file_name
+        return get_pokesprite_url_by_id(id, depth+1)
+
+if __name__ == "__main__":
+    #get_generation()
+    #read_dat_file()
+    get_sprite_by_id()
