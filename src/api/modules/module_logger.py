4320966495d04b07b04fc352c842d1bfdcfbe919
tsuna244 <gulcinoemre@gmail.com>
2025-05-08 21:05:04 +0200

    added documentation for logging class

--- src/api/modules/module_logger.py
+++ src/api/modules/module_logger.py
@@ -4,16 +4,32 @@ from time import strftime
 
 # singelton Logger Class
 class LoggerClass(object):
+    """
+        Class that allows to log with a configured format
+
+        Methods:
+            get_logger: returns a logger object
+    """
     _logger = None
     _instance = None
 
-    # ensure that there is only one instance of logger
     def __new__(cls, *args, **kwargs):
+        """Ensures that the logger class is a singelton
+
+        Returns:
+            logging.Logger: Logger from the logging class as singelton object
+        """
         if not cls._instance:
             cls._instance = super(LoggerClass, cls).__new__(cls, *args, **kwargs)
         return cls._instance
     
     def get_logger(self):
+        """Function that returns a configured logger
+
+        Returns:
+            logging.Logger: Logger object from the logging module that is configured with stream and file output
+                            File output only on linux systems
+        """
         if LoggerClass._logger is None:
         
             # set logging format
@@ -43,6 +59,14 @@ class LoggerClass(object):
 
 # utility function: log message for each function in specific format
 def log_function(module_name: str, function_name: str, log_msg: str, log_type = "info"):
+    """Utility function to help log functions with a better format
+
+    Args:
+        module_name (str): The name of the module that calls the log function
+        function_name (str): The name of the function inside the module that calls the log function
+        log_msg (str): the message that should be logged
+        log_type (str, optional): Type of logging: "error", "warn" or "info". Defaults to "info".
+    """
     log = LoggerClass().get_logger()
     msg = "({:15s} | {:30s}) -> {}".format(module_name, function_name + "(...)", log_msg)
     if log_type == "error":
