4fc42bbdb0627ef45eb7988f72188e8b25b4513d
tsuna244 <gulcinoemre@gmail.com>
2025-06-17 14:17:08 +0200

    testing out some configuration for playwrigth test

--- src/test/test_playwright.py
+++ src/test/test_playwright.py
@@ -2,7 +2,7 @@ import re
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
-    page.goto("http://127.0.0.1:8000/")
+    page.goto("http://0.0.0.0:8000/")
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Sqs2025"))
 
